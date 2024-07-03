2294c31121b3ebca4a96d83f0345802828613618
KeeganOReilly <keegan.oreilly@bbd.co.za>
2024-07-03 20:47:53 +0200

    Updated flyway scripts

--- Database/flyway/scripts/V001__create_tables.sql
+++ Database/flyway/scripts/V001__create_tables.sql
@@ -1,60 +1,56 @@
-CREATE TABLE
-   "HomeOwningStatus" (
-      "id" serial PRIMARY KEY,
-      "status_description" varchar(20)
-   );
-
-CREATE TABLE
-   "Businesses" (
-      "id" serial PRIMARY KEY,
-      "business_name" varchar(50),
-      "business_type" varchar(50)
-   );
-
-CREATE TABLE
-   "Personas" (
-      "id" bigserial PRIMARY KEY,
-      "next_of_kin_id" bigint,
-      "partner_id" bigint,
-      "parent_id" bigint,
-      "birth_format_time" varchar(10),
-      "hunger" integer,
-      "health" integer,
-      "adult" boolean,
-      "days_starving" integer,
-      "alive" boolean,
-      "sick" boolean,
-      "num_electronics_owned" integer,
-      "home_owning_status_id" integer REFERENCES "HomeOwningStatus" ("id") ON DELETE SET NULL
-   );
-
-CREATE TABLE
-   "StockItems" (
-      "id" serial PRIMARY KEY,
-      "persona_id" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
-      "business_id" integer REFERENCES "Businesses" ("id") ON DELETE SET NULL,
-      "num_stocks" integer,
-      "date_bought" varchar(10)
-   );
-
-CREATE TABLE
-   "FoodItems" (
-      "id" serial PRIMARY KEY,
-      "persona_id" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
-      "food_date_bought" varchar(10),
-      "food_stored_in_electronic" boolean,
-      "food_health" integer,
-      "food_eaten" boolean
-   );
-
-CREATE TABLE
-   "EventTypes" ("id" serial PRIMARY KEY, "event_name" varchar(8));
-
-CREATE TABLE
-   "EventsOccurred" (
-      "id" bigserial PRIMARY KEY,
-      "event_id" int REFERENCES "EventTypes" ("id") ON DELETE SET NULL,
-      "persona_id_1" bigint,
-      "persona_id_2" bigint,
-      "date_occurred" varchar(10)
-   );
+CREATE TABLE "HomeOwningStatus" (
+  "id" serial PRIMARY KEY,
+  "status_description" varchar(20)
+);
+ 
+CREATE TABLE "Businesses" (
+  "id" serial PRIMARY KEY,
+  "business_name" varchar(50),
+  "business_type" varchar(50)
+);
+ 
+CREATE TABLE "Personas" (
+  "id" bigserial PRIMARY KEY,
+  "next_of_kin_id" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
+  "partner_id" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
+  "parent_id" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
+  "birth_format_time" varchar(10),
+  "hunger" integer,
+  "health" integer,
+  "adult" boolean,
+  "days_starving" integer,
+  "alive" boolean,
+  "sick" boolean,
+  "num_electronics_owned" integer,
+  "home_owning_status_id" integer REFERENCES "HomeOwningStatus" ("id") ON DELETE SET NULL
+);
+ 
+CREATE TABLE "StockItems" (
+  "id" serial PRIMARY KEY,
+  "persona_id" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
+  "business_id" integer REFERENCES "Businesses" ("id") ON DELETE SET NULL,
+  "num_stocks" integer,
+  "date_bought" varchar(10)
+);
+ 
+CREATE TABLE "FoodItems" (
+  "id" serial PRIMARY KEY,
+  "persona_id" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
+  "food_date_bought" varchar(10),
+  "food_stored_in_electronic" boolean,
+  "food_health" integer,
+  "food_eaten" boolean
+);
+ 
+CREATE TABLE "EventTypes" (
+  "id" serial PRIMARY KEY,
+  "event_name" varchar(8)
+);
+ 
+CREATE TABLE "EventsOccurred" (
+  "id" bigserial PRIMARY KEY,
+  "event_id" int REFERENCES "EventTypes" ("id") ON DELETE SET NULL,
+  "persona_id_1" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
+  "persona_id_2" bigint REFERENCES "Personas" ("id") ON DELETE SET NULL,
+  "date_occurred" varchar(10)
+);
