c94fc98258f851996e5afad798ca695f735eec72
Farisani-Ratshikombo-BBD <farisanipaul.ratshikombo@vcontractor.co.za>
2024-07-03 13:24:41 +0200

    adding more persona endpoints

--- PersonaBackend/Controllers/PersonaController.cs
+++ PersonaBackend/Controllers/PersonaController.cs
@@ -104,7 +104,7 @@ namespace PersonaBackend.Controllers
         /// </summary>
         [HttpGet("getAlivePersonas")]
         //[ApiKeyAuthFilter("HandOfZeus")]
-        [ProducesResponseType(typeof(ApiResponse<PersonaIdList>), 200)]
+        [ProducesResponseType(typeof(ApiResponse<PersonaList>), 200)]
         [SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
         public async Task<IActionResult> GetAlivePersonas()
         {
@@ -112,14 +112,69 @@ namespace PersonaBackend.Controllers
             {
                 var alivePersonas = await _dbContext.Personas
                     .Where(p => p.Alive)
-                    .Select(p => p.Id)
                     .ToListAsync();
+
+                var response = new ApiResponse<PersonaList>
+                {
+                    Success = true,
+                    Message = "Alive personas retrieved",
+                    Data = new PersonaList { personas = alivePersonas }
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all persons, dead and alive.
+        /// </summary>
+        [HttpGet("getAllPersonas")]
+        //[ApiKeyAuthFilter("HandOfZeus")]
+        [ProducesResponseType(typeof(ApiResponse<PersonaList>), 200)]
+        //[SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
+        public async Task<IActionResult> GetAllPersonas()
+        {
+            try
+            {
+                var allPersonas = await _dbContext.Personas.ToListAsync();
+
+                var response = new ApiResponse<PersonaList>
+                {
+                    Success = true,
+                    Message = "List of all Personas",
+                    Data = new PersonaList { personas = allPersonas }
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all persons, dead and alive.
+        /// </summary>
+        [HttpGet("getAllPersonaID")]
+        //[ApiKeyAuthFilter("HandOfZeus")]
+        [ProducesResponseType(typeof(ApiResponse<PersonaIdList>), 200)]
+        //[SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
+        public async Task<IActionResult> GetAllPersonasID()
+        {
+            try
+            {
+                var allPersonas = await _dbContext.Personas.Select(s => s.Id).ToListAsync();
 
                 var response = new ApiResponse<PersonaIdList>
                 {
                     Success = true,
-                    Message = "Alive personas retrieved",
-                    Data = new PersonaIdList { PersonaIds = alivePersonas }
+                    Message = "List of all Personas",
+                    Data = new PersonaIdList { PersonaIds = allPersonas }
                 };
 
                 return Ok(response);
@@ -136,20 +191,18 @@ namespace PersonaBackend.Controllers
         [HttpGet("getChildlessPersonas")]
         //[ApiKeyAuthFilter("HandOfZeus")]
         [ProducesResponseType(typeof(ApiResponse<PersonaIdList>), 200)]
-        [SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
+        //[SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
         public async Task<IActionResult> GetChildlessPersonas()
         {
             try
             {
                 var childlessPersonas = await _dbContext.Personas
-                    .Where(p => p.ParentId == null)
-                    .Select(p => p.Id)
-                    .ToListAsync();
+                    .Where(p => p.ParentId == null).Select(s => s.Id).ToListAsync();
 
                 var response = new ApiResponse<PersonaIdList>
                 {
                     Success = true,
-                    Message = "Childless personas retrieved",
+                    Message = "Retrieve all childless personas",
                     Data = new PersonaIdList { PersonaIds = childlessPersonas }
                 };
 
@@ -165,16 +218,15 @@ namespace PersonaBackend.Controllers
         /// Retrieves all unmarried personas.
         /// </summary>
         [HttpGet("getSinglePersonas")]
-        //[ApiKeyAuthFilter("HandOfZeus")]
         [ProducesResponseType(typeof(ApiResponse<PersonaIdList>), 200)]
-        [SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
+        //[ApiKeyAuthFilter("HandOfZeus")]
         public async Task<IActionResult> GetSinglePersonas()
         {
             try
             {
                 var singlePersonas = await _dbContext.Personas
                     .Where(p => p.PartnerId == null)
-                    .Select(p => p.Id)
+                    .Select(s => s.Id)
                     .ToListAsync();
 
                 var response = new ApiResponse<PersonaIdList>
@@ -193,11 +245,97 @@ namespace PersonaBackend.Controllers
         }
 
         /// <summary>
+        /// Count all married personas
+        /// </summary>
+        [HttpGet("Married")]
+        [ProducesResponseType(typeof(ApiResponse<long>), 200)]
+        public async Task<IActionResult> GetMarriedPersonas()
+        {
+            try
+            {
+                var marriedPersons = await _dbContext.Personas
+                    .Where(p => p.PartnerId != null)
+                    .ToListAsync();
+
+                var response = new ApiResponse<long>
+                {
+                    Success = true,
+                    Message = "List of Married Persons",
+                    Data = marriedPersons.Count()
+                };
+
+                return Ok(response);
+            }
+            catch(Exception ex) 
+            {
+                return HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Count all birth since the beginning of time
+        /// </summary>
+        [HttpGet("Births")]
+        [ProducesResponseType(typeof(ApiResponse<long>), 200)]
+        //[SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
+        public async Task<IActionResult> GetTotalBirths()
+        {
+            try
+            {
+                var totalBirths = await _dbContext.EventsOccurred
+                    .Where(e => e.EventId == (int)EventTypeEnum.Born)
+                    .Select(s => s.PersonaId1).ToListAsync();
+
+                var response = new ApiResponse<long>
+                {
+                    Success = true,
+                    Message = "Total Persons born since the beginning of time",
+                    Data = totalBirths.Count()
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Get a list of dead people to date
+        /// </summary>
+        [HttpGet("DeadPersons")]
+        [ProducesResponseType(typeof(ApiResponse<PersonaList>), 200)]
+        //[SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
+        public async Task<IActionResult> GetDeceasedPersons()
+        {
+            try
+            {
+                var deadPersons = await _dbContext.Personas
+                    .Where(p => p.Alive == false)
+                    .ToListAsync();
+
+                var response = new ApiResponse<PersonaList>
+                {
+                    Success = true,
+                    Message = "List of Dead Persons",
+                    Data = new PersonaList { personas = deadPersons }
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        /// <summary>
         /// Gets list of personas that died between dates.
         /// </summary>
         [HttpGet("hasDied")]
         [ProducesResponseType(typeof(ApiResponse<PersonaIdList>), 200)]
-        [SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
+        //[SwaggerResponseExample(200, typeof(ApiResponsePersonaIdListEmptyExample))]
         public async Task<IActionResult> HasDied(string startDate, string endDate = null)
         {
             try
@@ -267,7 +405,7 @@ namespace PersonaBackend.Controllers
         /// </summary>
         [HttpGet("gotMarried")]
         [ProducesResponseType(typeof(ApiResponse<PersonaMarriageList>), 200)]
-        [SwaggerResponseExample(200, typeof(ApiResponseMarriedPairExample))]
+        //[SwaggerResponseExample(200, typeof(ApiResponseMarriedPairExample))]
         public async Task<IActionResult> GotMarried(string startDate, string endDate = null)
         {
             try
@@ -309,7 +447,7 @@ namespace PersonaBackend.Controllers
         /// </summary>
         [HttpGet("hadChild")]
         [ProducesResponseType(typeof(ApiResponse<ParentChildList>), 200)]
-        [SwaggerResponseExample(200, typeof(ApiResponseChildPairExample))]
+        //[SwaggerResponseExample(200, typeof(ApiResponseChildPairExample))]
         public async Task<IActionResult> HadChild(string startDate, string endDate = null)
         {
             try
@@ -335,6 +473,82 @@ namespace PersonaBackend.Controllers
                     Data = new ParentChildList
                     {
                         ParentChildren = parentChildPairs.ToList()
+                    }
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Post parent id to create a new child
+        /// </summary>
+        [HttpPost("makeNewChild")]
+        [ProducesResponseType(typeof(ApiResponse<Persona>), 200)]
+        //[SwaggerResponseExample(200, typeof(ApiResponseChildPairExample))]
+        public async Task<IActionResult> MakeNewChild(long parent_id)
+        {
+            try
+            {
+                var timeNow = DateTime.Now;
+
+                var newChild = new Persona
+                {
+                    BirthFormatTime = timeNow.ToString(),
+                    ParentId = parent_id,
+                    Hunger = 0, 
+                    Health = 100, 
+                    Adult = false,
+                    Alive = true, 
+                    Sick = false,
+                    NumElectronicsOwned = 0, 
+                    HomeOwningStatusId = null 
+                };
+
+                _dbContext.Personas.Add(newChild);
+                await _dbContext.SaveChangesAsync();
+
+                var response = new ApiResponse<Persona>
+                {
+                    Success = true,
+                    Message = "New child persona created successfully",
+                    Data = newChild
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Get all stocks related to a persona by id
+        /// </summary>
+        [HttpPost("getPersonaStocks")]
+        [ProducesResponseType(typeof(ApiResponse<StockItem>), 200)]
+        //[SwaggerResponseExample(200, typeof(ApiResponseChildPairExample))]
+        public async Task<IActionResult> GetPersonaStocks(long persona_id)
+        {
+            try
+            {
+                var stockItems = await _dbContext.StockItems
+                    .Where(s => s.PersonaId == persona_id)
+                    .ToListAsync();
+
+                var response = new ApiResponse<StockItem>
+                {
+                    Success = true,
+                    Message = "Stock items retrieved successfully",
+                    Data = new PersonaStocks
+                    {
+                        PersonaId = persona_id,
+                        Stocks = stockItems
                     }
                 };
 
