85151bd31b9ac1c7208d5448e854937b7eae3863
jennadunford <jenna.dunford@bbd.co.za>
2024-07-07 16:03:16 +0200

    initial stuff

---
+++ PersonaFrontend/config.js
@@ -0,0 +1,7 @@
+const clientId = '31o7mlelnoequ7ph13pt3tstnn';
+const cognitoDomain = 'persona-manager.auth.eu-west-1.amazoncognito.com';
+let backendUrl = 'https://api.persona.projects.bbdgrad.com';
+let redirectUri = 'https://persona.projects.bbdgrad.com/callback.html';
+let logoutRedirectUri = 'https://persona.projects.bbdgrad.com/logout.html';
+
+export {backendUrl, redirectUri, logoutRedirectUri, clientId, cognitoDomain };
